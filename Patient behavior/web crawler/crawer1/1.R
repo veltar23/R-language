beaff4bb57d2b346b682cc2236a337b0181273b0
veltar23 <r05741051@ntu.edu.tw>
2017-09-08 19:08:41 +0800

    170908

--- Patient behavior/web crawler/crawer1/1.R
+++
@@ -1,7 +0,0 @@
-
-load("fb_oauth")
-
-
-group<-getGroup(group_id=209561432418821, token=fb.oauth, n = 100, since = NULL, until = NULL,
-                feed = TRUE, api = NULL)
-write.csv(group,"group.csv")
